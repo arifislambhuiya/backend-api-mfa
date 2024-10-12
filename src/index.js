9cf22947cb363d72105f6880552024eabe6ceeeb
arifislambhuiya <arifulislam517@gmail.com>
2024-10-12 16:39:12 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import {app} from './app.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+dotenv.config({
+    path:"./.env"
+})
+
+//listen app
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8001, ()=>{
+    console.log(`Server is running on PORT ${process.env.PORT}`)
+})
+}).catch((error)=>{
+    console.log(`Database connection frild`,error)
+})
